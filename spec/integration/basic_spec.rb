95022e2e400893ba6b3297ee0ce27e94f05b90ce
Michael Ries <michael@riesd.com>
2012-05-06 06:11:00 +0200

    added failing spec for joining an existing game as a second client

--- spec/integration/basic_spec.rb
+++ spec/integration/basic_spec.rb
@@ -13,6 +13,12 @@ describe AIS::Server do
     c
   }
   
+  let(:client2) {
+    c = EventMachine::HttpRequest.new('ws://127.0.0.1:12345/').get :timeout => 1.0 
+    c.errback{ fail }
+    c
+  }
+  
   it "should accept websocket connections" do
     EM.run do
       ais.start 
@@ -67,7 +73,27 @@ describe AIS::Server do
     end
   end
   
-  it "should accept registrations for games"
+  it "should accept registrations for games" do
+    EM.run do
+      ais.start
+      EM.add_timer(0.1) do
+        client.stream do |msg|
+          msg = JSON.parse!(msg)
+          ["greeting","created"].should include(msg["type"])
+          client.send JSON.generate({:type => :create, :game => :test}) if msg["type"] == "greeting"
+          if msg["type"] == "created" then
+            client2.stream do |msg_guest|
+              msg_guest = JSON.parse!(msg_guest)
+              p msg_guest
+              ["greeting","joined"].should include(msg_guest["type"])
+              client2.send JSON.generate(:type => :join, :game => msg["game_id"]) if msg_guest["type"] == "greeting"
+              EM.stop if msg_guest["type"] == "registered"
+            end
+          end
+        end
+      end
+    end
+  end
   it "should generate ai players for games"
   it "should request a move from the client"
   it "should accept a move from the client"
